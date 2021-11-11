fd9a69090bde117ea4f6a49227c7dbf17a85a956
ngtienanh202 <81847154+ngtienanh202@users.noreply.github.com>
2021-11-12 00:09:19 +0700

    Update QLKS.cpp

--- QLKS.cpp
+++ QLKS.cpp
@@ -15,6 +15,12 @@ class NhanVien{
         virtual void Nhap();
         virtual void Xuat();
         virtual long TinhLuong() = 0;
+        virtual long getLuong(){
+            return TinhLuong();
+        }
+        virtual string getTen(){
+            return HoTen;
+        }
 };
 
 NhanVien::NhanVien(){
@@ -80,7 +86,7 @@ void NhanVien::Xuat(){
     cout << "Ho va ten: " << HoTen << endl;
     cout << "Nam vao lam: " << NamVaoLam << endl;
     cout << "So ngay lam: " << SoNgayLam << endl;
-    cout << "Tien luong: " << this->TinhLuong() << endl;
+    cout << "Tien luong: " << TinhLuong()<< endl;
 }
 
 long LeTan::TinhLuong(){
@@ -112,6 +118,7 @@ void LeTan::Nhap(){
 }
 
 void LeTan::Xuat(){
+	cout << "Chuc vu: Le tan." << endl;
 	NhanVien::Xuat();
 }
 
@@ -120,6 +127,7 @@ void BaoVe::Nhap(){
 }
 
 void BaoVe::Xuat(){
+	cout << "Chuc vu: Bao ve." << endl;
 	NhanVien::Xuat();
 }
 
@@ -128,6 +136,7 @@ void PhucVuPhong::Nhap(){
 }
 
 void PhucVuPhong::Xuat(){
+	cout << "Chuc vu: Phuc vu phong." << endl;
 	NhanVien::Xuat();
 }
 
@@ -136,15 +145,23 @@ void DauBep::Nhap(){
 }
 
 void DauBep::Xuat(){
+	cout << "Chuc vu: Dau bep." << endl;
 	NhanVien::Xuat();
 }
 
+
+
+
 class QuanLyNhanVien{
 	private:
-		vector<NhanVien*> dsNhanVien;
+		vector<NhanVien *> dsNhanVien;
 	public:
 		void NhapDS();
 		void XuatDS();
+        void SapXep();
+        void MaxLong();
+        void TongLuong();
+        void TimKiem_NV();
 };
 
 void QuanLyNhanVien::NhapDS(){
@@ -161,7 +178,7 @@ void QuanLyNhanVien::NhapDS(){
         cin >> k;
         switch(k){
         	case 1:
-                nv = new LeTan;
+                nv = new LeTan; 
                 nv->Nhap();
                 dsNhanVien.push_back(nv); 
                 break;
@@ -180,8 +197,8 @@ void QuanLyNhanVien::NhapDS(){
                 nv->Nhap();
 				dsNhanVien.push_back(nv);
                 break;
-            case 0:
-            	break;
+            default:
+				break;
 		}
 	} while (k != 0);
 }
@@ -193,7 +210,61 @@ void QuanLyNhanVien::XuatDS(){
 	}
 }
 
-int main(){
+//Sap xep nhan vien theo luong tang dan 
+void QuanLyNhanVien::SapXep(){
+    for(int i=0;i<dsNhanVien.size();i++){
+        for(int j=i+1;j<dsNhanVien.size();j++){
+            if(dsNhanVien.at(i)->getLuong()>dsNhanVien.at(j)->getLuong()){
+                NhanVien *t=dsNhanVien.at(i);
+                dsNhanVien.at(i)=dsNhanVien.at(j);
+                dsNhanVien.at(j)=t;
+            }
+        }
+    }
+    XuatDS();
+}
+//Nhan vien co luong cao nhat
+void QuanLyNhanVien::MaxLong(){
+    NhanVien *max=dsNhanVien.at(0);
+    for(int i=1;i<dsNhanVien.size();i++){
+        if(dsNhanVien.at(i)->getLuong() > max->getLuong())
+            max = dsNhanVien.at(i);
+    }
+    cout<<endl<<"Nhan Vien co Luong cao nhat: "<<endl;
+    max->Xuat();
+}
+
+//Tim nhan vien theo ten
+void QuanLyNhanVien::TimKiem_NV(){
+    NhanVien *t=dsNhanVien.at(0);
+    string TimTenNV;
+    int Found=0,i=0;
+    cout<<"Nhap ten Nhan Vien can tim: ";
+    cin.ignore();
+    getline(cin,TimTenNV);
+    for( i=0;i<dsNhanVien.size();i++){
+        if(dsNhanVien.at(i)->getTen() == TimTenNV){
+            t = dsNhanVien.at(i);
+            cout<<"\n==================="<<endl;
+            t->Xuat();
+        break;
+        }
+    }
+    if(i==dsNhanVien.size()){
+    cout<<"\n========================="<<endl;
+    cout<<"Khong tim thay Nhan vien nay !!"<<endl;
+    }
+}
+
+//Tong tien khach san tra luong cho nhan vien
+void QuanLyNhanVien::TongLuong(){
+    long tong=0;
+    for(int i=0;i<dsNhanVien.size();i++){
+        tong += dsNhanVien.at(i)->getLuong();
+    }
+    cout<<endl<<"Tong tien khach san tra luong cho nhan vien: "<<tong<< " VND" << endl;
+}
+int Menu(){
 	int k;
     QuanLyNhanVien qlnv;
 	do{
@@ -201,6 +272,10 @@ int main(){
         cout << "* * * * * QUAN LY NHAN VIEN KHACH SAN * * * * *" << endl;
         cout << "1. Nhap nhan vien" << endl;
         cout << "2. Xuat nhan vien." << endl;
+        cout << "3. Sap Xep Luong tang dan."<< endl;
+        cout << "4. Nhan vien co Luong cao nhat."<<endl;
+        cout << "5. Tim kiem theo ten Nhan Vien."<<endl;
+        cout << "6. Tong luong Nhan Vien. "<<endl;
         cout << "0. Thoat chuong trinh." << endl;
         cout << "Chon chuc nang: ";
         cin >> k;
@@ -210,11 +285,32 @@ int main(){
                 break;
             case 2:
             	qlnv.XuatDS();
+                system("pause");
     			break;
+            case 3:
+                qlnv.SapXep();
+                system("pause");
+                break;
+            case 4:
+                qlnv.MaxLong();
+                system("pause");
+                break;
+            case 5:   
+                qlnv.TimKiem_NV();
+                system("pause");
+                break;
+            case 6:
+                qlnv.TongLuong();
+                system("pause");
+                break;
     		default:
 				break;
         }
     } while(k != 0);
+}
+
+int main(){
+	Menu();
 }
 
 
